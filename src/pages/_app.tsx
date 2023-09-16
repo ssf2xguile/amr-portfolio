8e0be7cba684d2796d5312a7e852bd5bc04cd566
ssf2xguile <iwanatteiwanai123@gmail.com>
2023-09-16 23:49:57 +0900

    フォントをNoto Sans JPに変更する
    柔らかい雰囲気のページとして閲覧することができる

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import theme from '@/libs/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       {/* ラップされたアプリケーションのコンポーネントを表示する */}
       <Component {...pageProps} />
     </ChakraProvider>
